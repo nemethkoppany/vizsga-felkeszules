cd057a03d795221c7981cdbe4d650625cae454a9
nemethkoppany <koppany344@gmail.com>
2026-04-19 15:04:20 +0200

    Gyakorlas

--- backend/vizsga_gyakorlas_13/src/controller.ts
+++ backend/vizsga_gyakorlas_13/src/controller.ts
@@ -28,3 +28,91 @@ export const login = async(req:any, res:any) =>{
         return res.status(500).json("Szerver hiba");
     }
 }
+
+export const getProducts = async (_req:any, res:any) =>{
+    const connection = await mysql.createConnection(config.database);
+
+    try{
+
+        const [result]:any = await connection.query(`
+            SELECT * FROM products
+            `)
+
+
+            return res.status(200).json(result);
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json("Szerver hiba")
+    }
+}
+
+export const getProductsbyId = async (req:any, res:any) =>{
+    const connection = await mysql.createConnection(config.database);
+    const id = Number(req.params.id);
+    try{
+
+        const [result]:any = await connection.query(`
+            SELECT * FROM products WHERE id = ?
+            `,[id])
+
+            if(result.length === 0){
+                return res.status(404).json("Az elem nem létezik");
+            }
+            
+            return res.status(200).json(result);
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json("Szerver hiba")
+    }
+}
+
+export const postProducts = async(req:any, res:any ) =>{
+    const connection = await mysql.createConnection(config.database);
+
+    if(!req.body.name || !req.body.description || !Number(req.body.price) || !Number(req.body.stock)){
+        return res.status(401).json("Hiányzó vagy hibás mezők");
+    }
+    try{
+        const [result]:any = await connection.query(`
+                INSERT INTO products VALUES(NULL,?,?,?,?,?)
+            `,[req.body.name, req.body.description, Number(req.body.price), Number(req.body.stock), req.body.pictureurl])
+
+            if(result.affectedRows === 0){
+                return res.status(400).json("Az adatok nem megfelelőek");
+            }
+
+            return res.status(201).json({id: result.insertId});
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json("Szerver hiba");
+    }
+}
+
+export const putProducts = async(req:any, res:any )=>{
+    const connection = await mysql.createConnection(config.database);
+
+    if(!req.body.name || !req.body.description || !Number(req.body.price) || !Number(req.body.stock)){
+        return res.status(400).json("Az adatok nem megfelelőek!");
+    }    
+
+    try{
+        const [result]:any = await connection.query(`
+            UPDATE products SET name=?,description=?,price=?,stock=?,pictureurl=? WHERE id = ?
+            `,[req.body.name, req.body.description, Number(req.body.price), Number(req.body.stock), req.body.pictureurl, Number(req.params.id)])
+
+            if(result.affectedRows === 0){
+                return res.status(404).json("Az elem nem létezik");
+            
+            
+            }
+
+            return res.status(201).json("Sikeres módosytás");
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json("Szerver hiba")
+    }
+}
--- backend/vizsga_gyakorlas_13/src/routes.ts
+++ backend/vizsga_gyakorlas_13/src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { login } from "./controller";
+import { login,getProducts,getProductsbyId,postProducts,putProducts } from "./controller";
+import { verifyToken } from "./auth";
 
 
 const router = Router();
 
 router.post("/api/login",login);
-
+router.get("/api/products",getProducts);
+router.get("/api/products/:id",getProductsbyId);
+router.post("/api/products",verifyToken,postProducts);
+router.put("/api/products/:id",verifyToken,putProducts);
 export default router;
